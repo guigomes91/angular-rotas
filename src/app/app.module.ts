12a00a60c85f7675300722a9e69ca57fd70e3edc
Guilherme Gomes <guilherme.gomes91@outlook.com>
2023-05-31 19:52:08 -0300

    Ajuste de link ativo de rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MaterializeModule } from 'angular2-materialize';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
