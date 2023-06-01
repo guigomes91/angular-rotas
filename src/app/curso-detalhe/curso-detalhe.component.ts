0766e73eca587b62b0708e202b4c695a4b09fd06
Guilherme Gomes <guilherme.gomes91@outlook.com>
2023-06-01 07:27:33 -0300

    Inscrição de rotas

--- src/app/curso-detalhe/curso-detalhe.component.ts
+++ src/app/curso-detalhe/curso-detalhe.component.ts
@@ -1,16 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-curso-detalhe',
   templateUrl: './curso-detalhe.component.html',
   styleUrls: ['./curso-detalhe.component.css']
 })
-export class CursoDetalheComponent {
+export class CursoDetalheComponent implements OnInit {
 
   id: string = '';
 
-  constructor(private route: ActivatedRoute) {
-    this.id = this.route.snapshot.params['id'];
+  constructor(private route: ActivatedRoute,
+    private inscricao: Subscription) {
+    //this.id = this.route.snapshot.params['id'];
+    //console.log(this.route);
+  }
+
+  ngOnInit(): void {
+    this.inscricao = this.route.params.subscribe(
+      (params: any) => {
+        this.id = params['id'];
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.inscricao.unsubscribe();
   }
 }
