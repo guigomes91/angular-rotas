95491ff3a17471062bf4425f199fbe652a509094
Guilherme Gomes <guilherme.gomes91@outlook.com>
2023-06-05 21:35:44 -0300

    lazy loading de módulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AlunosModule } from './alunos/alunos.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CursosModule } from './cursos/cursos.module';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
@@ -17,8 +15,6 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-    CursosModule,
-    AlunosModule,
     BrowserAnimationsModule,
     AppRoutingModule,
   ],
